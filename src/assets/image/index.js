4a6736edd54135da2745719db7f750c813c79767
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-09-21 14:14:55 -0300

    feat: finalized header

---
+++ src/assets/image/index.js
@@ -0,0 +1,6 @@
+export {default as ArrowDown} from './arrow-down.svg';
+export {default as Logo} from './logo-default.svg';
+export {default as LogoOutline} from './logo-outline.svg';
+export {default as Dribble} from './dribble.svg';
+export {default as Figma} from './figma.svg';
+export {default as Github} from './github.svg';
